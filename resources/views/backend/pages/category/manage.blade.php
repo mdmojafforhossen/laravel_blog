a1abd1927de01481c40f24796234c349fa2f0b69
mdmojaffor98@gmail.com <mdmojaffor98@gmail.com>
2024-08-21 19:02:35 +0600

    add git ignore

---
+++ resources/views/backend/pages/category/manage.blade.php
@@ -0,0 +1,55 @@
+@extends('backend.layout')
+
+@section("body-content")
+	<section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Category Management</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th >#</th>
+                      <th>Task</th>
+                      <th>Progress</th>
+                      <th >Label</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                   
+                    <tr>
+                      <td>2.</td>
+                      <td>Clean database</td>
+                      <td>age</td>
+                      <td>20</td>
+                    </tr>
+                    
+                    
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer clearfix">
+                <ul class="pagination pagination-sm m-0 float-right">
+                  <li class="page-item"><a class="page-link" href="#">«</a></li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item"><a class="page-link" href="#">»</a></li>
+                </ul>
+              </div>
+            </div>
+            <!-- /.card -->
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+          <!-- /.col -->
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,22 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Frontend\FrontendController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Frontend Route Start
+Route::get('/','App\Http\Controllers\Frontend\FrontendController@homepage');
+Route::get('/contact','App\Http\Controllers\Frontend\FrontendController@contact')->name('name.contact');
+Route::get('/category','App\Http\Controllers\Frontend\FrontendController@category')->name('name.category');
+// Backend Route Start
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('/dashboard','App\Http\Controllers\Backend\BackendController@dashboard')->name('name.dashboard');
+    // category route
+    Route::group(['prefix'=>'category'],function(){
+    Route::get('manage','App\Http\Controllers\Backend\CategoryController@manage')->name('category.manage');
+    });
 });
-
+// -------------------
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
